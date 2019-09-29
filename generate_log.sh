0129ff9161f931b0a08bfe328e1b9e1839ac1add
Roman Frołow <rofrol@gmail.com>
2019-09-29 19:04:49 +0200

    Use actual line for generating

--- generate_log.sh
+++ generate_log.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for i in {1..64000}; do echo 'a a a [a] "A a" 0 0 "a" ""' >> test.log; done
+for i in {1..64000}; do echo '54.213.178.139 - - [24/Sep/2019:03:28:55 +0200] "GET / HTTP/1.1" 301 415 "-" "Go-http-client/1.1"' >> test.log; done
